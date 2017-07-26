b0d7a15f714ed0cf3330249acb12ac5dd18f76d9
viorelaioia <viorelaioia@gmail.com>
2017-07-26 18:13:34 +0300

    Set the login tests to use counter-based OTPs

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,11 +15,25 @@ def ldap(variables):
 
 
 @pytest.fixture
-def passcode(secret_seed):
-    totp = pyotp.TOTP(secret_seed)
-    return totp.now()
+def passcode(secret_seed, counter):
+    hotp = pyotp.HOTP(secret_seed)
+    return hotp.at(counter+1)
 
 
 @pytest.fixture
 def urls(variables):
     return variables['RPs_urls']
+
+
+@pytest.fixture
+def write_in_file(filename, content):
+    f = open(filename, 'w')
+    f.write(str(content))
+    f.close()
+
+
+@pytest.fixture
+def file_content(filename):
+    if not open(filename, 'r').read():
+        return 0
+    return open(filename, 'r').read()
--- tests/test_login_to_rps.py
+++ tests/test_login_to_rps.py
@@ -8,18 +8,24 @@ class TestLogin:
 
     def test_login_reps(self, selenium, ldap, urls):
         test = Reps(selenium, urls['reps'])
-        test.login_with_ldap(ldap['email'], ldap['password'], conftest.passcode(ldap['secret_seed']))
+        counter = int(conftest.file_content("index"))
+        test.login_with_ldap(ldap['email'], ldap['password'], conftest.passcode(ldap['secret_seed'], counter))
+        conftest.write_in_file("index", counter + 1)
         assert test.is_logout_button_displayed
         test.click_logout()
 
     def test_login_slack(self, selenium, ldap, urls):
         test = Slack(selenium, urls['slack'])
-        test.login_with_ldap(ldap['email'], ldap['password'], conftest.passcode(ldap['secret_seed']))
+        counter = int(conftest.file_content("index"))
+        test.login_with_ldap(ldap['email'], ldap['password'], conftest.passcode(ldap['secret_seed'], counter))
+        conftest.write_in_file("index", counter + 1)
         assert test.is_username_displayed
         test.logout()
 
     def test_login_moderator(self, selenium, ldap, urls):
         test = Moderator(selenium, urls['moderator'])
-        test.login_with_ldap(ldap['email'], ldap['password'], conftest.passcode(ldap['secret_seed']))
+        counter = int(conftest.file_content("index"))
+        test.login_with_ldap(ldap['email'], ldap['password'], conftest.passcode(ldap['secret_seed'], counter))
+        conftest.write_in_file("index", counter + 1)
         assert test.is_logout_button_displayed
         test.click_logout()
