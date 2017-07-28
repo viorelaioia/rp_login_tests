4127a9f497e8e41da4abebb9c45c26752c0cb96b
viorelaioia <viorela.ioia@softvision.ro>
2017-07-28 10:31:22 +0300

    Set the login tests to use counter-based OTPs (#3)

--- tests/test_login_to_rps.py
+++ tests/test_login_to_rps.py
@@ -6,20 +6,23 @@ from tests import conftest
 
 class TestLogin:
 
-    def test_login_reps(self, selenium, ldap, urls):
+    def test_login_reps(self, selenium, ldap, urls, counter_api):
         test = Reps(selenium, urls['reps'])
-        test.login_with_ldap(ldap['email'], ldap['password'], conftest.passcode(ldap['secret_seed']))
+        test.login_with_ldap(ldap['email'], ldap['password'],
+                             conftest.passcode(ldap['secret_seed'], conftest.increase_otp_counter(counter_api)))
         assert test.is_logout_button_displayed
         test.click_logout()
 
-    def test_login_slack(self, selenium, ldap, urls):
+    def test_login_slack(self, selenium, ldap, urls, counter_api):
         test = Slack(selenium, urls['slack'])
-        test.login_with_ldap(ldap['email'], ldap['password'], conftest.passcode(ldap['secret_seed']))
+        test.login_with_ldap(ldap['email'], ldap['password'],
+                             conftest.passcode(ldap['secret_seed'], conftest.increase_otp_counter(counter_api)))
         assert test.is_username_displayed
         test.logout()
 
-    def test_login_moderator(self, selenium, ldap, urls):
+    def test_login_moderator(self, selenium, ldap, urls, counter_api):
         test = Moderator(selenium, urls['moderator'])
-        test.login_with_ldap(ldap['email'], ldap['password'], conftest.passcode(ldap['secret_seed']))
+        test.login_with_ldap(ldap['email'], ldap['password'],
+                             conftest.passcode(ldap['secret_seed'], conftest.increase_otp_counter(counter_api)))
         assert test.is_logout_button_displayed
         test.click_logout()
