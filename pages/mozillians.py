b76cefb6e3ac282fe6a56a449d5309cd186ffcb5
viorelaioia <viorelaioia@gmail.com>
2017-07-31 14:35:27 +0300

    Implement login tests for mozillians, discourse, standups, sso dashboard, service-now stage

---
+++ pages/mozillians.py
@@ -0,0 +1,40 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+from pages.auth0 import Auth0
+from pages.base import Base
+
+
+class Mozillians(Base):
+    _sign_in_button = (By.ID, 'nav-login')
+    _logout_menu_item_locator = (By.ID, 'nav-logout')
+    _profile_menu_locator = (By.CSS_SELECTOR, '#nav-main > a.dropdown-toggle')
+    _username_locator = (By.CSS_SELECTOR, '.username')
+    _dropdown_menu_locator = (By.CSS_SELECTOR, 'ul.dropdown-menu')
+
+    def __init__(self, selenium, url):
+        super(Mozillians, self).__init__(selenium)
+        self.go_to_url(url)
+
+    @property
+    def is_logout_menu_item_present(self):
+        return self.is_element_present(*self._logout_menu_item_locator)
+
+    @property
+    def is_username_displayed(self):
+        return self.is_element_visible(*self._username_locator)
+
+    def login_with_ldap(self, email, password, passcode):
+        self.selenium.find_element(*self._sign_in_button).click()
+        auth = Auth0(self.selenium)
+        auth.login_with_ldap(email, password, passcode)
+
+    def click_options(self):
+        self.selenium.find_element(*self._profile_menu_locator).click()
+        WebDriverWait(self.selenium, self.timeout).until(
+            lambda s: self.selenium.find_element(*self._dropdown_menu_locator))
+
+    def click_logout(self):
+        self.click_options()
+        self.selenium.find_element(*self._logout_menu_item_locator).click()
+        WebDriverWait(self.selenium, self.timeout).until(lambda s: not self.is_logout_menu_item_present)
---
+++ pages/sso_dashboard.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+
+from pages.auth0 import Auth0
+from pages.base import Base
+
+
+class SsoDashboard(Base):
+
+    _profile_icon_locator = (By.CSS_SELECTOR, '.profile-icon')
+    _logout_button_locator = (By.CSS_SELECTOR, 'a[href="/logout"]')
+
+    def __init__(self, selenium, url):
+        super(SsoDashboard, self).__init__(selenium)
+        self.go_to_url(url)
+
+    @property
+    def is_profile_icon_displayed(self):
+        return self.is_element_visible(*self._profile_icon_locator)
+
+    def login_with_ldap(self, email, password, passcode):
+        auth = Auth0(self.selenium)
+        auth.login_with_ldap(email, password, passcode)
+
+    def logout(self):
+        self.selenium.find_element(*self._profile_icon_locator).click()
+        self.wait_for_element_visible(*self._logout_button_locator)
+        self.selenium.find_element(*self._logout_button_locator).click()
